a4a190ebe7c3c2aa6b1d5feca744d8341b02fc2e
laima <laima.busaite@gmail.com>
2021-03-09 13:48:55 +0200

    adc7driver

---
+++ adc7driver.py
@@ -0,0 +1,109 @@
+# This program will test interactions with the ADC 7 Click board
+# from a Raspberry Pi.
+#
+# First, we need to load the necessary modules. 
+
+import signal               # module to allow us to interrupt program
+import sys                  # system module (to exit program)
+import Rpi.GPIO as GPIO     # RPi GPIO module
+import spidev               # SPI module
+import struct               # change bytes to integers
+
+# We need to put our data somewhere. We will be lazy for now
+# and make a global variable for the file handle.
+
+myDataFileName='datafile.txt'
+myDataFile=open(myDataFileName,"w")
+
+# Then we will define a callback that allows us to interrupt
+# the program by pressing <CTRL-C>
+
+def signal_handler(sig, frame):
+    global myDataFile
+    myDatafile.close()      # close the data file
+    GPIO.cleanup()          # do some cleanup
+    spi.close()
+    sys.exit(0)             # exit program
+
+# Next we will define a callback function to read the 
+# data from the ADC 7 Click when the DRL signal indicates
+# that data is ready.
+
+def readADC7_callback(channel):
+    # This is an edge event callback function!
+    # It is run in a different thread to the main program.
+    # 
+    # We will be lazy and put the data in a global variable
+    global myDataFile
+    data=spi.readbytes(4)
+    dataword=struct.unpack("<I", bytearray(data))
+    myDataFile.write('d\n',dataword)
+
+# Now we are ready to start the main program.
+
+if __name__ == '__main__':
+# First, we will configure the GPIO pins for working with the
+# ADC 7 Click installed on position 1 of the Click Shield.
+
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(5,GPIO.OUT)  # RST. Used for Filter Preset Enable (PRE)
+    GPIO.setup(6,GPIO.IN)   # INT. Busy Indicator (BSY)
+    GPIO.setup(18,GPIO.OUT) # PTM. Sampling Trigger (MCK)
+
+# We leave pin GPIO13 as is for now. We need to write to it, in order
+# to load a new control word into the LTC2500-32 chip, but we need 
+# to read from it in order to know when Data is ready.
+
+# Next, we configure the SPI interface.
+
+    dev1 = spidev.SpiDev()        # Create instance of list
+    spi.max_speed = 10000000      # SPI clock speed 10 MHz
+    spi.lsbfirst = False          # Send MSB as first bit
+    spi.open(0,0)                 # Open Connection bus 0, device 0
+# The first 0 is the bus number. We have only one SPI bus on the 
+# RPi, so it must always be 0. The second parameter is the device
+# number/chip select os CS(CS0 or CS1). RPI supports two SPI
+# devices. CS0 controls the shield connectors. CS1 controls the
+# additional 12-bit ADC on the click shield. 
+
+# Now we define a callback function for reading data when
+# the DRL sees a rising edge. 
+
+
+    
+
+# Now we will send a control word. 
+    GPIO.setup(13,GPIO.OUT)       # We DRL to start communication
+    GPIO.output(18,GPIO.LOW)      # The MCK should be 0.
+    GPIO.output(13,GPIO.LOW)      # To start sending data, set DRL to 0.
+# We will select the Averaging filter (0111) with downsampling
+# factor DF32 (0101). 
+    send = [0x0b,0x57]            # Data word: 1011 0101 0111
+    spi.xfer(send)                # Send our word to LTC2500.
+
+# Now we need to change DRL back to read, so it can tell serve 
+# as Data Ready Indicator and tell us when data is ready. 
+
+# Next we setup the PWM for pulse width modulation
+# Let's start with a trigger rate of 250 kHz. I think it should 
+# be possible to increase to 1 MHz, but I'm not sure.
+# The important thing is that there is enough time to send the 
+# data before the next event is ready.
+# For 250 Khz, that time is 4 us. 
+# With an SPI clock at 10 MHz, it takes 32*100ns=3.2us to send
+# 32 bytes, so it's close. Might be safer to try slower 
+# ADC trigger or faster SPI. Need to experiment.  
+
+    pwm = GPIO.PWM(18, 250000)    # Set PWM 250 kHz.
+    pwm.start(0.1)                # Start with duty cycle 10%.
+# I believe the minimum pulse width is 4 ns, so 10% duty 
+# cycle should be conservative.
+
+# Next we add an edge detection function to call our callback
+# function when DRL switches from 0 to 1.
+
+    channel = 13                  # DRL pin for click shield 1
+    GPIO.add_event_detect(channel, GPIO.RISING, callback=readADC7_callback)  # add rising edge detection on a channel
+
+    signal.signal(signal.SIGING, signal_handler)
+    signal.pause()
