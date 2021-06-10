b8c02407c0ba662978b793d2acc6d034fa75c741
Magnetometer <reinis.lazda@lu.lv>
2021-06-10 16:56:06 +0100

    weighted mean

--- adc_synthNV.py
+++ adc_synthNV.py
@@ -26,32 +26,34 @@ import string
 os.system("clear")
 
 # read parameters
-filename = "ODMR_fit_parameters.json"
-a_file = open(filename, "r")
-parameters = json.load(a_file)
+#filename = "ODMR_fit_parameters.json"
+#a_file = open(filename, "r")
+#parameters = json.load(a_file)
 # parameters = dict(parameters)
 # print(parameters)
 
-print("ODMR fit parameters")
-D = parameters["D"] #2867.61273
-Mz_array = np.array([parameters["Mz1"], parameters["Mz2"], parameters["Mz3"], parameters["Mz4"]]) #np.array([1.85907453, 2.16259814, 1.66604227, 2.04334145]) #np.array([7.32168327, 6.66104172, 9.68158138, 5.64605102])
-B_lab = np.array([parameters["B_labx"], parameters["B_laby"], parameters["B_labz"]]) #np.array([169.121512, 87.7180839, 40.3986877]) #np.array([191.945068, 100.386360, 45.6577322])
+#print("ODMR fit parameters")
+#D = parameters["D"] #2867.61273
+#Mz_array = np.array([parameters["Mz1"], parameters["Mz2"], parameters["Mz3"], parameters["Mz4"]]) #np.array([1.85907453, 2.16259814, 1.66604227, 2.04334145]) #np.array([7.32168327, 6.66104172, 9.68158138, 5.64605102])
+#B_lab = np.array([parameters["B_labx"], parameters["B_laby"], parameters["B_labz"]]) #np.array([169.121512, 87.7180839, 40.3986877]) #np.array([191.945068, 100.386360, 45.6577322])
 
-print('D =', D)
-print('Mz_array =', Mz_array)
-print('B_lab =', B_lab)
+#print('D =', D)
+#print('Mz_array =', Mz_array)
+#print('B_lab =', B_lab)
 
 # NV center orientation in laboratory frame
 # (100)
-nv_center_set = nv.NVcenterSet(D=D, Mz_array=Mz_array)
-nv_center_set.setMagnetic(B_lab=B_lab)
+#nv_center_set = nv.NVcenterSet(D=D, Mz_array=Mz_array)
+#nv_center_set.setMagnetic(B_lab=B_lab)
 # print(nv_center_set.B_lab)
 
-frequencies0 = nv_center_set.four_frequencies(np.array([2000, 3500]), nv_center_set.B_lab)
+#frequencies0 = nv_center_set.four_frequencies(np.array([2000, 3500]), nv_center_set.B_lab)
 # print(frequencies0)
 
-A_inv = nv_center_set.calculateAinv(nv_center_set.B_lab)
+#A_inv = nv_center_set.calculateAinv(nv_center_set.B_lab)
 # print(A_inv)
+
+frequencies0, A_inv = nv.calculateInitialSystem(filename = "ODMR_fit_parameters.json")
 
 spi = spidev.SpiDev()
 spi.open(1,2)
@@ -320,13 +322,22 @@ for i in range(sets):
     #    peaks, amplitudes = detect_peaks(dataframe['MW'], dataframe['ODMR'], debug=False)
     #    #print(peaks)
     #    peaks_list.append(peaks)
-    peaks, amplitudes = detect_peaks(peak2_MW, peak2_ODMR, debug=False)
+    #peaks, amplitudes = detect_peaks_weighted(peak2_MW, peak2_ODMR, debug=False)
+    #peaks_list.append(peaks)
+    #peaks, amplitudes = detect_peaks_weighted(peak4_MW, peak4_ODMR, debug=False)
+    #peaks_list.append(peaks)
+    #peaks, amplitudes = detect_peaks_weighted(peak6_MW, peak6_ODMR, debug=False)
+    #peaks_list.append(peaks)
+    #peaks, amplitudes = detect_peaks_weighted(peak8_MW, peak8_ODMR, debug=False)
+    #peaks_list.append(peaks)
+
+    peaks = detect_peaks_weighted(peak2_MW, peak2_ODMR)
     peaks_list.append(peaks)
-    peaks, amplitudes = detect_peaks(peak4_MW, peak4_ODMR, debug=False)
+    peaks = detect_peaks_weighted(peak4_MW, peak4_ODMR)
     peaks_list.append(peaks)
-    peaks, amplitudes = detect_peaks(peak6_MW, peak6_ODMR, debug=False)
+    peaks = detect_peaks_weighted(peak6_MW, peak6_ODMR)
     peaks_list.append(peaks)
-    peaks, amplitudes = detect_peaks(peak8_MW, peak8_ODMR, debug=False)
+    peaks = detect_peaks_weighted(peak8_MW, peak8_ODMR)
     peaks_list.append(peaks)
 
 #def import_data(filename):
@@ -341,6 +352,8 @@ for i in range(sets):
 
     peaks_list = np.array(peaks_list).flatten()
     # print(peaks_list)
+    if (peaks_list == 0).any():
+        continue
 
     delta_frequencies = frequencies0 - peaks_list #peaks[1::2]
     #print("\ndelta F =",delta_frequencies)
