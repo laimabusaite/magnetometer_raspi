b607680b117755364c02292baca0fa701069c723
Magnetometer <reinis.lazda@lu.lv>
2021-06-29 15:25:46 +0100

    udp communication

--- plot1.py
+++ plot1.py
@@ -5,7 +5,7 @@ import numpy as np
 import time
 import os
 
-filenames = sorted(glob.glob("test123/*.dat"))
+filenames = sorted(glob.glob("udp_test/*.dat"))
 filenames.sort(key=os.path.getmtime)
 #filenames = filenames[-4*10::1]
 
