0005f333060dbadc6325412d27ce7e2c0b2ee241
Magnetometer <reinis.lazda@lu.lv>
2021-06-29 17:46:41 +0100

    log files in data folder

--- meassure1.py
+++ meassure1.py
@@ -150,13 +150,13 @@ def write_file(x,y,folder_name,s1,s2=""):
     return
 
 
-def write_file_log(s1,s2):
+def write_file_log(foldername,s1,s2):
     '''
     Write log messages durring the execution of the program
     Outputs:
         Log message file
     '''
-    filename1="test_data_logs/{0:s}.log".format(s1)
+    filename1="{0:s}/{1:s}.log".format(foldername,s1)
 
     f1 = open(filename1, 'a')
     #for i2 in range(len(x)):
@@ -463,7 +463,7 @@ while True:
             Bsens = deltaB_from_deltaFrequencies(A_inv, delta_frequencies)
             print("Bxyz = ({1:.2f}, {2:.2f}, {3:.2f}) G\t\t|B| = {4:.2f} G".format(i1+1,Bsens[0],Bsens[1],Bsens[2],np.sqrt(Bsens[0]*Bsens[0]+Bsens[1]*Bsens[1]+Bsens[2]*Bsens[2])), flush = True)
             result1 = "{0:.6f}\t{1:.6f}\t{2:.6f}\t{3:.6f}".format(Bsens[0],Bsens[1],Bsens[2],np.sqrt(Bsens[0]*Bsens[0]+Bsens[1]*Bsens[1]+Bsens[2]*Bsens[2]))
-            write_file_log(log_file_name, result1)
+            write_file_log(foldername1,log_file_name, result1)
             B1[i1] = np.sqrt(Bsens[0]*Bsens[0]+Bsens[1]*Bsens[1]+Bsens[2]*Bsens[2])
             B1x[i1] = Bsens[0]
             B1y[i1] = Bsens[1]
@@ -481,7 +481,7 @@ while True:
         print("\n-----------------------------------------------------------")
         print("\nTotal time {0:.1f} s    Measurements {1:d}/{2:d} = {3:.0f} %    Rate {4:.2f} Hz".format(t-t0,i1,sets,100.0*i1/sets,sets/(t-t0)))
         rate1 = "Total time {0:.1f} s    Measurements {1:d}/{2:d} = {3:.0f} %    Rate {4:.2f} Hz".format(t-t0,i1,sets,100.0*i1/sets,sets/(t-t0))
-        write_file_log(log_file_name+"_summary",rate1)
+        write_file_log(foldername1,log_file_name+"_summary",rate1)
 
         Bmean = np.mean([B1[i] for i in range(i1)])
         Bstd = np.std([B1[i] for i in range(i1)])
@@ -500,10 +500,10 @@ while True:
         std_error1y = "By = ({0:.2f} \u00B1 {1:.2f}) G\trBy = {2:.2f} %".format(Bymean, Bystd, np.abs((Bystd/Bymean)*100))
         print("\nBz = ({0:.2f} \u00B1 {1:.2f}) G\trBz = {2:.2f} %".format(Bzmean, Bzstd, np.abs((Bzstd/Bzmean)*100)))
         std_error1z = "Bz = ({0:.2f} \u00B1 {1:.2f}) G\trBz = {2:.2f} %".format(Bzmean, Bzstd, np.abs((Bzstd/Bzmean)*100))
-        write_file_log(log_file_name+"_summary",std_error1)
-        write_file_log(log_file_name+"_summary",std_error1x)
-        write_file_log(log_file_name+"_summary",std_error1y)
-        write_file_log(log_file_name+"_summary",std_error1z)
+        write_file_log(foldername1,log_file_name+"_summary",std_error1)
+        write_file_log(foldername1,log_file_name+"_summary",std_error1x)
+        write_file_log(foldername1,log_file_name+"_summary",std_error1y)
+        write_file_log(foldername1,log_file_name+"_summary",std_error1z)
 
         print("\n###########################################################\n")
         # ----
