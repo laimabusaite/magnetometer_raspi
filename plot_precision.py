729467c6bfdcba78626f3d549afc4103d978064b
Laima <laima.busaite@gmail.com>
2021-07-16 16:00:15 +0300

    fit precision sqrt

--- plot_precision.py
+++ plot_precision.py
@@ -10,9 +10,17 @@ def exponential_fit(x, a, b, c):
 def parabola_fit(x, a, b, c):
     return a * x**2 + b * x + c
 
+def sqrt_fit(x, a, b):
+    return a / np.sqrt(b*x)
+
+def pow_fit(x, a, b, c):
+    return a * (b*x)**c
+
 def fit_func(x, a, b, c):
     # return parabola_fit(x, a, b, c)
-    return exponential_fit(x, a, b, c)
+    # return exponential_fit(x, a, b, c)
+    return sqrt_fit(x, a, b)
+
 if __name__ == '__main__':
 
     filename = 'tables/table_sensitivity.csv'
@@ -40,8 +48,8 @@ if __name__ == '__main__':
                    'avg', 'dev (MHz)', 'point count']
 
 
-    for dev in dev_set[1:2]:
-        for B_index in B_index_set[1:2]:
+    for dev in dev_set[:]:
+        for B_index in B_index_set[:]:
             dataframe_dev = dataframe[(dataframe['dev (MHz)'] == dev) & (dataframe['B index'] == B_index)][column_list]
             dataframe_dev.sort_values(by='avg', inplace=True)
             print(dataframe_dev)
@@ -50,22 +58,34 @@ if __name__ == '__main__':
             y = dataframe_dev['Bmod measured stderr (mT)']
             plt.figure(f'B {B_index}, dev {dev} MHz', figsize=(6, 4))
             plt.title(f'B = {B_index * 0.1} mT, dev = {dev} MHz')
-            plt.plot(x, y, marker='o')
             try:
-                fitting_parameters, covariance = curve_fit(fit_func, x, y)
+                x_fit = np.linspace(0, 200, 1000)
+                fitting_parameters, covariance = curve_fit(exponential_fit, x, y)
                 a, b, c = fitting_parameters
-                x_fit = np.linspace(0, 150, 1000)
-                y_fit = fit_func(x_fit, a, b, c)
-                plt.plot(x_fit, y_fit, 'r')
-                # next_x = 100
-                # next_y = exponential_fit(next_x, a, b, c)
-                # plt.plot(np.append(x, next_x), np.append(y, next_y), 'ro')
+                print('exp', fitting_parameters)
+                y_fit = exponential_fit(x_fit, a, b, c)
+                plt.plot(x_fit, y_fit, 'r', label=fr'${a:.4f} \cdot \exp{{(-{b:.4f}\cdot x)}} + {c:.4f}$')
+
+                fitting_parameters, covariance = curve_fit(sqrt_fit, x, y)
+                a, b = fitting_parameters
+                print('sqrt', fitting_parameters)
+                y_fit = sqrt_fit(x_fit, a, b)
+                plt.plot(x_fit, y_fit, 'g', label=fr'$\dfrac{{{a:.2f}}}{{\sqrt{{{b:.2f} x}}}}$')
+
+                fitting_parameters, covariance = curve_fit(pow_fit, x, y)
+                a, b, c = fitting_parameters
+                print('pow', fitting_parameters)
+                y_fit = pow_fit(x_fit, a, b, c)
+                plt.plot(x_fit, y_fit, 'k', label=fr'${a:.3f} \cdot ({b:.3} x)^{{{c:.3f}}}$')
             except Exception as e:
                 print(e)
+            plt.plot(x, y, marker='o', ms=7)
             plt.xlabel('Number of averages')
             plt.ylabel(r'Bmod measured stderr (mT)')
-            plt.xlim(0, 150)
+            plt.xlim(0, 200)
+            plt.ylim(0.0,0.002)
             plt.tight_layout()
+            plt.legend()
             plt.savefig(f'fig_std_avg/fit_B{B_index}_dev{dev:.0f}MHz.png')
             plt.savefig(f'fig_std_avg/fit_B{B_index}_dev{dev:.0f}MHz.pdf')
 
@@ -73,20 +93,33 @@ if __name__ == '__main__':
             y = dataframe_dev['Bmod measured stderr (mT)']/dataframe_dev['Bmod measured (mT)'] * 100
             plt.figure(f'rel B {B_index}, dev {dev} MHz', figsize=(6, 4))
             plt.title(f'B = {B_index * 0.1:.1f} mT, dev = {dev} MHz')
-            plt.plot(x, y, marker='o')
+
             try:
-                fitting_parameters, covariance = curve_fit(fit_func, x, y)
+                fitting_parameters, covariance = curve_fit(exponential_fit, x, y)
                 a, b, c = fitting_parameters
                 x_fit = np.linspace(0, 150, 1000)
-                y_fit = fit_func(x_fit, a, b, c)
+                y_fit = exponential_fit(x_fit, a, b, c)
                 plt.plot(x_fit, y_fit, 'r')
+
+                fitting_parameters, covariance = curve_fit(sqrt_fit, x, y)
+                a, b = fitting_parameters
+                x_fit = np.linspace(0, 150, 1000)
+                y_fit = sqrt_fit(x_fit, a, b)
+                plt.plot(x_fit, y_fit, 'g')
+
+                fitting_parameters, covariance = curve_fit(pow_fit, x, y)
+                a, b, c = fitting_parameters
+                x_fit = np.linspace(0, 150, 1000)
+                y_fit = pow_fit(x_fit, a, b, c)
+                plt.plot(x_fit, y_fit, 'k')
             except Exception as e:
                 print(e)
+            plt.plot(x, y, marker='o', ms=10)
             plt.xlabel('Number of averages')
             plt.ylabel(r'$\Delta B / B$ (%)')
             plt.xlim(0, 150)
             plt.tight_layout()
-            plt.savefig(f'fig_std_avg/fit_rel_B{B_index}_dev{dev:.0f}MHz.png')
-            plt.savefig(f'fig_std_avg/fit_rel_B{B_index}_dev{dev:.0f}MHz.pdf')
+            # plt.savefig(f'fig_std_avg/fit_rel_B{B_index}_dev{dev:.0f}MHz.png')
+            # plt.savefig(f'fig_std_avg/fit_rel_B{B_index}_dev{dev:.0f}MHz.pdf')
 
     plt.show()
